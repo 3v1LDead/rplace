0801bb3146e6158a6e907983b18236de737f3990
3v1LDead <ws031@dd7an.de>
2023-07-23 21:53:58 +0200

    overlay reihenfolge geändert

--- overlay.user.js
+++ overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Schagges Community + Gronkh + Bonjwa + r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      16
+// @version      17
 // @description  try to take over the canvas! Combination of Schagges, Gronkh, Bonjwa and r/placeDE template
 // @author       Chris-GW + nama17 + Kloroller_DE + vertigo + Sockenschuh (for Bonjwa), 3v1LDead (for Schagges)
 // @match        https://garlic-bread.reddit.com/*
@@ -10,10 +10,10 @@
 // @downloadURL  https://github.com/3v1LDead/rplace/raw/main/overlay.user.js
 // ==/UserScript==
 
-addOverlayImage("https://raw.githubusercontent.com/3v1LDead/rplace/main/overlay.png")
-addOverlayImage("https://raw.githubusercontent.com/FeLuckLP/rplace/main/overlay.png")
-addOverlayImage("https://raw.githubusercontent.com/rplacebonjwa/rplace/main/overlay.png")
 addOverlayImage("https://place.army/overlay_target.png")
+addOverlayImage("https://raw.githubusercontent.com/rplacebonjwa/rplace/main/overlay.png")
+addOverlayImage("https://raw.githubusercontent.com/FeLuckLP/rplace/main/overlay.png")
+addOverlayImage("https://raw.githubusercontent.com/3v1LDead/rplace/main/overlay.png")
 
 function addOverlayImage(url) {
   var overlayImage = null;
